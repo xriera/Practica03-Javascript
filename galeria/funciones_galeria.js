5934df93bb0872114b7168c1895b4ef1467ddd91
xavier <xavier@192.168.0.101>
2019-05-23 02:37:55 -0500

    funciones gaelria 1

--- galeria/funciones_galeria.js
+++ galeria/funciones_galeria.js
@@ -5,16 +5,16 @@
  */
 
 
-array = [{ "id": 1, "imagen": "Imagenes/I1.jpg" },
-{ "id": 2, "imagen": "Imagenes/I2.jpg" },
-{ "id": 3, "imagen": "Imagenes/I3.jpg" },
-{ "id": 4, "imagen": "Imagenes/I4.jpg" },
-{ "id": 5, "imagen": "Imagenes/I5.jpg" },
-{ "id": 6, "imagen": "Imagenes/I6.jpg" },
-{ "id": 7, "imagen": "Imagenes/I7.jpg" },
-{ "id": 8, "imagen": "Imagenes/I8.jpg" },
-{ "id": 9, "imagen": "Imagenes/I9.jpg" },
-{ "id": 10, "imagen": "Imagenes/I10.jpg" }];
+array = [{ "id": 1, "imagen": "imagenes/img1.jpg" },
+{ "id": 2, "imagen": "imagenes/img2.jpg" },
+{ "id": 3, "imagen": "imagenes/img3.jpg" },
+{ "id": 4, "imagen": "imagenes/img4.jpg" },
+{ "id": 5, "imagen": "imagenes/img5.jpg" },
+{ "id": 6, "imagen": "imagenes/img6.jpg" },
+{ "id": 7, "imagen": "imagenes/img7.jpg" },
+{ "id": 8, "imagen": "imagenes/img8.jpg" },
+{ "id": 9, "imagen": "imagenes/img9.jpg" },
+{ "id": 10, "imagen": "imagenes/img10.jpg" }];
 imagenes = [1, 2, 3, 4, 5];
 
 var posActual = 0;
