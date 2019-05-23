112bb23f1d8e5d8ddcaf6e6aeac88fb9017b93e8
xavier <xavier@192.168.0.101>
2019-05-23 02:35:21 -0500

    funciones gaelria

--- galeria/funciones_galeria.js
+++ galeria/funciones_galeria.js
@@ -5,3 +5,56 @@
  */
 
 
+array = [{ "id": 1, "imagen": "Imagenes/I1.jpg" },
+{ "id": 2, "imagen": "Imagenes/I2.jpg" },
+{ "id": 3, "imagen": "Imagenes/I3.jpg" },
+{ "id": 4, "imagen": "Imagenes/I4.jpg" },
+{ "id": 5, "imagen": "Imagenes/I5.jpg" },
+{ "id": 6, "imagen": "Imagenes/I6.jpg" },
+{ "id": 7, "imagen": "Imagenes/I7.jpg" },
+{ "id": 8, "imagen": "Imagenes/I8.jpg" },
+{ "id": 9, "imagen": "Imagenes/I9.jpg" },
+{ "id": 10, "imagen": "Imagenes/I10.jpg" }];
+imagenes = [1, 2, 3, 4, 5];
+
+var posActual = 0;
+
+function inicio() {
+    for (i = 0; i < 5; i++) {
+        imagenes[i] = Math.floor(Math.random() * (10));
+    }
+    posActual = 0;
+    verificar();
+    imprimir();
+}
+
+function imprimir() {
+    console.log(imagenes);
+    var texto = "<img src=" + array[imagenes[posActual]].imagen + ">";
+    window.document.getElementById("imagen").innerHTML = texto;
+}
+function verificar() {
+    if (posActual == 4) {
+        window.document.getElementById("siguiente").disabled = true;
+        window.document.getElementById("anterior").disabled = false;
+    }
+    else if (posActual == 0) {
+        window.document.getElementById("anterior").disabled = true;
+        window.document.getElementById("siguiente").disabled = false;
+    } else {
+        window.document.getElementById("siguiente").disabled = false;
+        window.document.getElementById("anterior").disabled = false;
+    }
+}
+
+function siguiente() {
+    posActual++;
+    verificar();
+    imprimir();
+}
+
+function anterior() {
+    posActual--;
+    verificar();
+    imprimir();
+}
